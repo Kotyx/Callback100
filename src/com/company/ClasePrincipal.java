52f8473aaa50bba99ef1eb631ed6bedc51af49be
Alvaro <alvaro.g.a.99@gmail.com>
2020-11-05 18:39:48 +0100

    update

--- src/com/company/ClasePrincipal.java
+++ src/com/company/ClasePrincipal.java
@@ -3,13 +3,13 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Random;
 public class ClasePrincipal implements Callback{
-
-    Thread1 [] t1=new Thread1[5];
+    final int TAM=100;
+    Thread1 [] t1=new Thread1[TAM];
     ArrayList <String> lhilos=new ArrayList<String>();
     public ClasePrincipal() {
         for(int i=1;i<=5;i++){
             System.out.println("Iteracion "+i);
-            for(int j=0;j<5;j++){
+            for(int j=0;j<TAM;j++){
                 Thread1 h = new Thread1(this);
                 h.setName("hilo "+j);
                 h.start();
@@ -25,7 +25,7 @@ public class ClasePrincipal implements Callback{
             }
             System.out.println("El hilo que ha terminado primero es: "+ lhilos.get(0));
             lhilos.clear();
-            if(contador()>25){
+            if(contador()>TAM){
                 System.out.println("Los hilos se han terminado");
             }
 
